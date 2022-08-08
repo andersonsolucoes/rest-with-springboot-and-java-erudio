d284c3e3d83f6f09108979e00b750fbff038e917
Anderson Florêncio <andersonflorencio@mandacarutecnologia.com.br>
2022-08-08 03:03:03 -0300

    Commit content JWT Authentication

--- Projeto/rest-with-springboot-and-java-erudio/src/main/java/br/com/spring/example/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ Projeto/rest-with-springboot-and-java-erudio/src/main/java/br/com/spring/example/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.spring.example.exceptions.ExceptionResponse;
+import br.com.spring.example.exceptions.InvalidJwtAuthenticationException;
 import br.com.spring.example.exceptions.RequiredObjectIsNullException;
 import br.com.spring.example.exceptions.ResourceNotFoundException;
 
@@ -34,6 +35,12 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {	
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationExceptions(Exception ex, WebRequest request) {	
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 	
 }
---
+++ Projeto/rest-with-springboot-and-java-erudio/src/main/java/br/com/spring/example/services/AuthServices.java
@@ -0,0 +1,59 @@
+package br.com.spring.example.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.spring.example.data.vo.v1.secutiry.AccountCredentialsVO;
+import br.com.spring.example.data.vo.v1.secutiry.TokenVO;
+import br.com.spring.example.repositories.UserRepository;
+import br.com.spring.example.security.jwt.JwtTokenProvider;
+
+@Service
+public class AuthServices {
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public ResponseEntity<TokenVO> signin(AccountCredentialsVO data) {
+		try {
+			var username = data.getUsername();
+			var password = data.getPassword();
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+			
+			var user = repository.findByUsername(username);
+			var tokenResponse = new TokenVO();
+			if(user != null) {
+				tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+			} else {
+				throw new UsernameNotFoundException("Username " + username + " not found!");
+			}
+			
+			return ResponseEntity.ok(tokenResponse);
+		} catch(Exception e) {
+			throw new BadCredentialsException("Invalid username/password supplied!");
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public ResponseEntity refreshToken(String username, String refreshToken) {
+		var user = repository.findByUsername(username);
+		
+		var tokenResponse = new TokenVO();
+		if (user != null) {
+			tokenResponse = tokenProvider.refreshToken(refreshToken);
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found!");
+		}
+		return ResponseEntity.ok(tokenResponse);
+	}
+}
